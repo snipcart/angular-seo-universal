54b134742f30bc0eb233c311dad94fec82a80199
Michael Poirier-Ginter <michael.poirierginter@gmail.com>
2018-07-10 09:49:38 -0400

    Fixed URL in product component.

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { PRODUCTS } from '../mocked-products';
-import { Product } from '../Product';
+import { Product } from '../product';
 import { Location } from '@angular/common';
 
 
@@ -20,7 +20,7 @@ export class ProductComponent implements OnInit {
   constructor(private route: ActivatedRoute, private location: Location) {
     const id = this.route.snapshot.paramMap.get('id');
     this.product = this.findProductById(id);
-    this.url = `${this.location.path()}`;
+    this.url = `https://snipcart-angular-universal.herokuapp.com/${this.location.path()}`;
   }
 
   ngOnInit() { }
